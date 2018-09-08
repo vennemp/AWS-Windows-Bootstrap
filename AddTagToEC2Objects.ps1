5cf44fa05cdc59f8350fd0b0b74ec672c29cb430
vennemp <vennemp@gmail.com>
2018-09-07 22:45:11 -0400

    Update AddTagToEC2Objects.ps1
    Changed to Out-GridView Format

--- AddTagToEC2Objects.ps1
+++ AddTagToEC2Objects.ps1
@@ -1,25 +1,9 @@
 ipmo *aws*
 
-$accesskey = Read-Host -Prompt "Insert your API Access Key"
-$secretkey = Read-Host -Prompt "Insert your API Secret Key"
-Set-AWSCredentials -StoreAs usersession  -AccessKey $accesskey -SecretKey $secretkey
-$wildcardfilter = Read-Host -Prompt "Enter filter string (include stars)"
-$ObjectType = Read-Host -Prompt "Object type (instance/volume)"
-$TagName = Read-Host -prompt "Enter tag name (NOT VALUE)"
-$TagValue = Read-Host -Prompt "Enter tag value"
-$tag = New-Object Amazon.EC2.Model.Tag
-$tag.key = $TagName
-$tag.value = $TagValue  
+##This assumes you have default region and credentials stored in session. See Initialize-AWSDefaultConfiguration...
 
-function TagEC2Object {
-
-$objects =  Get-EC2Tag -Region us-east-1 -StoredCredentials usersession | where {$_.resourcetype -eq $ObjectType -and $_.value -like $wildcardfilter -or $_.Value -contains $wildcardfilter} #contains is if your tag value has spaces as the like operator may not work for filters with spaces
-
-foreach ($object in $objects.ResourceId)
-{
-New-EC2Tag -Resources $object -Tags $tag -Region us-east-1 -storedcredentials usersession
-}
-}
-
-TagEC2Object
-Clear-AWSCredentials -StoredCredentials usersession
+(Get-EC2Tag) | 
+  Out-GridView -OutputMode Multiple | 
+  ForEach-Object -Process { 
+    New-EC2Tag -Tag (New-Object -TypeName Amazon.EC2.Model.Tag -ArgumentList @('Key', 'Value')) -Resource $PSItem.ResourceId
+  }
